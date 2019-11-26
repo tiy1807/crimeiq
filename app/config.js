05be28ca79aab419b66e88572c76ca89b4c15501
tiy1807 <thomasyems+github@gmail.com>
2019-11-26 19:39:44 +0000

    Road Traffic Incident service

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Report a Road Traffic Collision',
+  serviceName: 'Report a Road Traffic Incident',
 
   // Default port that prototype runs on
   port: '3000',
