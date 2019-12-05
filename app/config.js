b05ba7bca43de6c7850033637fc38fb398fb78bb
tiy1807 <thomasyems+github@gmail.com>
2019-12-05 15:36:47 +0000

    Added overarching splash page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Report a Road Traffic Incident',
+  serviceName: '',
 
   // Default port that prototype runs on
   port: '3000',
