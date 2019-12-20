d4743593f6d9e74628ac813955ffd28a563475f0
tiy1807 <thomasyems+github@gmail.com>
2019-12-20 12:28:10 +0000

    Added choosing location by map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,23 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+var mymap = window.L.map('mapid').setView([51.505, -0.09], 13);
+
+window.L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoidHllbXMiLCJhIjoiY2szeTZueHR6MGl1aDNscWNzOHMxaTBjOSJ9.NDdp3cYXLIxzlGuO7ejyGw'
+}).addTo(mymap);
+
+var marker = {}
+
+function onMapClick(e) {
+    if (marker != undefined) {
+        mymap.removeLayer(marker)
+    };
+    marker = window.L.marker(e.latlng).addTo(mymap);
+}
+
+mymap.on('click', onMapClick);
