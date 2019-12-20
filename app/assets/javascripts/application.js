2fe9d8f1185407aa8c172f413608a8e6f04b38d7
tiy1807 <thomasyems+github@gmail.com>
2019-12-20 13:33:34 +0000

    Added live feed from data.police.uk source to show nearby incidents

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,22 +9,61 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-var mymap = window.L.map('mapid').setView([51.505, -0.09], 13);
+var x = document.getElementById('mapid')
+if (document.getElementById('mapid')) {
+    var mymap = window.L.map('mapid').setView([51.505, -0.09], 13);
 
-window.L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: 'pk.eyJ1IjoidHllbXMiLCJhIjoiY2szeTZueHR6MGl1aDNscWNzOHMxaTBjOSJ9.NDdp3cYXLIxzlGuO7ejyGw'
-}).addTo(mymap);
+    window.L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        accessToken: 'pk.eyJ1IjoidHllbXMiLCJhIjoiY2szeTZueHR6MGl1aDNscWNzOHMxaTBjOSJ9.NDdp3cYXLIxzlGuO7ejyGw'
+    }).addTo(mymap);
 
-var marker = {}
+    var marker = {}
 
-function onMapClick(e) {
-    if (marker != undefined) {
-        mymap.removeLayer(marker)
-    };
-    marker = window.L.marker(e.latlng).addTo(mymap);
+    function onMapClick(e) {
+        if (marker != undefined) {
+            mymap.removeLayer(marker)
+        };
+        marker = window.L.marker(e.latlng).addTo(mymap);
+        localStorage.setItem("myGPSlat",e.latlng.lat.toFixed(6))
+        localStorage.setItem("myGPSlong",e.latlng.lng.toFixed(6))
+    }
+
+    mymap.on('click', onMapClick);
 }
 
-mymap.on('click', onMapClick);
+if (document.getElementById('incidents')) {
+    var xmlHttp = new XMLHttpRequest();
+    lat = localStorage.getItem("myGPSlat")
+    long = localStorage.getItem("myGPSlong")
+    if (lat == null){
+      lat = "52.629729"
+    }
+    if (long == null) {
+      long = "-1.131592"
+    }
+    window.console.info(lat)
+    window.console.info(long)
+
+    xmlHttp.open("GET", "https://data.police.uk/api/crime-last-updated", false);
+    xmlHttp.send(null)
+    date = JSON.parse(xmlHttp.responseText)["date"].substring(0,7)
+    window.console.info(date)
+
+    xmlHttp.open("GET", "https://data.police.uk/api/crimes-at-location?date=" + date + "&lat=" + lat + "&lng=" + long, false ); // false for synchronous request
+    xmlHttp.send( null );
+    response = JSON.parse(xmlHttp.responseText)
+    display_text = ""
+    if (response.length == 0) {
+      display_text = "No nearby incidents"
+    } else {
+      for (var i = 0; i < response.length; i++) {
+        window.console.info(response[i])
+        window.console.info(response.length)
+        display_text += response[i]["month"] + " - " + response[i]["category"] + " - " + response[i]["location"]["street"]["name"] + "<br>"
+      }
+    }
+    document.getElementById("incidents").innerHTML = display_text
+}
