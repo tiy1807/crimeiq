a69826f070e0b6316f1b150277b33c84fd7d4aea
tiy1807 <thomasyems+github@gmail.com>
2019-12-26 08:36:35 +0000

    Split out questions across multiple pages, to improve the journey flow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
 var x = document.getElementById('mapid')
 if (document.getElementById('mapid')) {
-    var mymap = window.L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = window.L.map('mapid').setView([53.96, -1.0873], 13);
 
     window.L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
