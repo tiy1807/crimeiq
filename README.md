df481863171781a89e5dac713a89dff6f813734b
ty-rx <thomasyems@redcross.org.uk>
2020-02-01 08:44:40 +0000

    Integrated with GOV UK Notify

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # crimeiq
-A repository for us to collaborate an create prototype
+Working as a team of young professionals on public sector problems on the TechConnect programme run by Tech UK.
+
+This is a framework to be able to report non-emergency incidents, without needing to know which particular organisation is responsible for responding.
+
+The report vandalism route is the most developed route.
+
+This is developed from the GOV.UK Prototype Kit v9.3.0, and integrated with GOV.UK Notify.
+
+If you have any questions about this piece of work, then please get in touch,
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,71 @@
 const express = require('express')
 const router = express.Router()
 
+var NotifyClient = require('notifications-node-client').NotifyClient,
+notify = new NotifyClient(process.env.NOTIFYAPIKEY);
+
+// The URL here needs to match the URL of the page that the user is on
+// when they type in their email address
+router.post('/vandalism/personal', function (req, res) {
+
+  notify.sendEmail(
+    // this long string is the template ID, copy it from the template
+    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+    // in your code.
+    '80518213-9ab1-43b2-9368-027c8cd40292',
+    // `emailAddress` here needs to match the name of the form field in
+    // your HTML page
+    process.env.email,
+    {
+      personalisation: {
+        incident: "graffiti",
+        street_name: "Farringdon Street",
+        name: "Thomas Yems",
+        date: "19/02/2020",
+        council: "City of London Council",
+        reference: "HDJ2123F"
+      },
+      reference: "HDJ2123F"
+    }
+  );
+
+  // This is the URL the users will be redirected to once the email
+  // has been sent
+  res.redirect('/vandalism/confirmation');
+
+});
+
+notify2 = new NotifyClient(process.env.NOTIFYAPIKEY);
+
+router.get('/vandalism/send-progress-email', function (req, res) {
+
+  notify2.sendEmail(
+    // this long string is the template ID, copy it from the template
+    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+    // in your code.
+    'e2f0b136-0ada-44fd-9d8d-a1db6c40fcc5',
+    // `emailAddress` here needs to match the name of the form field in
+    // your HTML page
+    process.env.email,
+    {
+      personalisation: {
+        incident: "graffiti",
+        street_name: "Farringdon Street",
+        name: "Thomas Yems",
+        date: "19/02/2020",
+        council: "City of London Council",
+        reference: "HDJ2123F"
+      },
+      reference: "HDJ2123F"
+    }
+  );
+
+  // This is the URL the users will be redirected to once the email
+  // has been sent
+  res.redirect('/vandalism/progress');
+
+});
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
